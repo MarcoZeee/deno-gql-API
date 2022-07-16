6fb37ca9fc7b53005a0f4713e91dac33ca14105c
MarcoZeee <MarcoZeee@github.com>
2022-07-17 01:39:37 +0800

    feat: better interface

--- types.ts
+++ types.ts
@@ -1,13 +1,13 @@
 export type GQLRequest = {
-  url: string
-  method: string
-  headers: Headers
-  json: () => Promise<any>
-}
+  url: string;
+  method: string;
+  headers: Headers;
+  json: () => Promise<any>;
+};
 
 export interface Product {
-  id: string
-  name: string
-  price: number
-  description: string
+  id: string;
+  name: string;
+  price: number;
+  description: string;
 }
